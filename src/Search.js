45be7adcc3ec6d9834b3e84ca72315fc3e561fc5
Taylor Thornton <thornrae@gmail.com>
2022-01-15 15:59:13 -0800

    basic set up for deployment

---
+++ src/Search.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
+class Search extends React.Component {
+  render() {
+    return (
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    );
+  }
+}
+
+export default Search;
